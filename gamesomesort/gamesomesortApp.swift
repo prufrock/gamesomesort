f832e70b219e69845859fd4c1663195fea097fad
David Kanenwisher <david.kanenwisher@projectphoenix.io>
2025-05-29 16:54:34 -0500

    initial commit

---
+++ gamesomesort/gamesomesortApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamesomesortApp.swift
+//  gamesomesort
+//
+//  Created by David Kanenwisher on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct gamesomesortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gamesomesortTests/gamesomesortTests.swift
@@ -0,0 +1,16 @@
+//
+//  gamesomesortTests.swift
+//  gamesomesortTests
+//
+//  Created by David Kanenwisher on 5/29/25.
+//
+
+import Testing
+
+struct gamesomesortTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
