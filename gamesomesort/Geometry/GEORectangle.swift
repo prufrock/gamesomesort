ca72d68a5b5ce148cc4d4a3db59838e67789f325
David Kanenwisher <d.kanen@gmail.com>
2025-06-19 21:59:05 -0500

    Add contains testing to GEORectangle

--- gamesomesort/Geometry/GEORectangle.swift
+++ gamesomesort/Geometry/GEORectangle.swift
@@ -8,6 +8,30 @@
 struct GEORectangle {
   var min, max: F2
 
+  var area: Float {
+    get {
+      height * width
+    }
+  }
+
+  var height: Float {
+    get {
+      max.y - min.y
+    }
+  }
+
+  var width: Float {
+    get {
+      max.x - min.x
+    }
+  }
+
+  private var corners: [F2] {
+    get {
+      [F2(min.x, min.y), F2(min.x, max.y), F2(max.x, max.y), F2(max.x, min.y)]
+    }
+  }
+
   init(min: F2, max: F2) {
     self.min = min
     self.max = max
@@ -23,7 +47,7 @@ struct GEORectangle {
     max = F2(maxX, maxY)
   }
 
-  func intersection(with rect: GEORectangle) -> F2? {
+  func intersection(with rect: Self) -> F2? {
     let left = F2(x: max.x - rect.min.x, y: 0)  // world
     if left.x <= 0 {
       return nil
@@ -43,5 +67,23 @@ struct GEORectangle {
 
     // sort by length with the smallest first and grab that one
     return [left, right, up, down].sorted(by: { $0.length < $1.length }).first
+  }
+
+  func contains(_ rect: Self) -> Bool {
+    // This can't contain the rectangle if it's not big enough
+    if area < rect.area {
+      return false
+    }
+
+    // All 4 corners must be inside this rectangle.
+    return rect.corners.filter { contains($0) }.count == 4
+  }
+
+  func contains(_ minX: Float, _ maxX: Float, _ minY: Float, _ maxY: Float) -> Bool {
+    contains(Self.init(minX, maxX, minY, maxY))
+  }
+
+  func contains(_ p: Float2) -> Bool {
+    !(min.x > p.x || max.x < p.x || min.y > p.y || max.y < p.y)
   }
 }
--- gamesomesortTests/Geometry/GEORectangleTests.swift
+++ gamesomesortTests/Geometry/GEORectangleTests.swift
@@ -25,4 +25,23 @@ struct GEORectangleTests {
 
     #expect(rectOne.intersection(with: rectTwo)!.x >= 0.1)
   }
+
+  @Test func testContainsFloat2() {
+    let r = GEORectangle(min: Float2(0.0, 0.0), max: Float2(5.0, 5.0))
+
+    #expect(r.contains(Float2(6.0, 6.0)) == false)
+    #expect(r.contains(Float2(6.0, 4.0)) == false)
+    #expect(r.contains(Float2(1.0, 1.0)) == true)
+  }
+
+  @Test func testContainsRect() {
+    let r = GEORectangle(0.0, 0.0, 5.0, 5.0)
+
+    #expect(r.contains(-1.0, -1.0, 1.0, 1.0) == false)  // upper left
+    #expect(r.contains(4.0, 1.0, 6.0, -1.0) == false)  // upper right
+    #expect(r.contains(-1.0, 4.0, 1.0, 6.0) == false)  // lower left
+    #expect(r.contains(4.0, 4.0, 6.0, 6.0) == false)  // lower right
+
+    #expect(r.contains(0.0, 0.0, 3.0, 3.0) == true)
+  }
 }
