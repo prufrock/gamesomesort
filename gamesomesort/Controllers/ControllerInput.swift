a0245588848f7482de40131d9b57742c4e440b37
David Kanenwisher <d.kanen@gmail.com>
2025-06-27 22:07:02 -0500

    Use a dedicated cancellable with debug output on completion

--- gamesomesort/Controllers/ControllerInput.swift
+++ gamesomesort/Controllers/ControllerInput.swift
@@ -13,7 +13,7 @@ class ControllerInput {
   let config: AppCoreConfig
 
   private var tapLocationSubject: PassthroughSubject<CGPoint, Never> = PassthroughSubject<CGPoint, Never>()
-  private var cancellables = Set<AnyCancellable>()
+  private var tapLocationCancelable: AnyCancellable?
 
   private var lastTouchedTime: Double = 0.0
   private var touchCoords: F2 = F2()
@@ -23,16 +23,32 @@ class ControllerInput {
     setupTapLocationSubscribers()
   }
 
+  /// Only initializes the subject if the cancellable does not exist. If for some reason tapping stops working it may because it was cancelled when
+  /// the view was backgrounded or something. Keep an eye out for issues with that and you may learn something!
   private func setupTapLocationSubscribers() {
-    tapLocationSubject
+    if tapLocationCancelable != nil {
+      return
+    }
+
+    tapLocationCancelable =
+      tapLocationSubject
       .debounce(for: .milliseconds(config.game.tapDelay), scheduler: RunLoop.main)
-      .sink { [weak self] location in
-        guard let self else { return }
-        self.touchCoords = F2(location.x.f, location.y.f)
-        self.lastTouchedTime = CACurrentMediaTime()
-        print("recieved tap at \(location)")
-      }
-      .store(in: &cancellables)
+      .sink(
+        receiveCompletion: { completion in
+          fatalError(
+            "ControllerInput::tapLocationSubject received completion: \(completion)."
+              + "You left this here, so you would know when the subscription dies."
+              + "A likely fix might be to either nil the cancellable or store in a set of cancellables."
+              + "Either way you need to make sure the subscription is started again."
+          )
+        },
+        receiveValue: { [weak self] location in
+          guard let self else { return }
+          self.touchCoords = F2(location.x.f, location.y.f)
+          self.lastTouchedTime = CACurrentMediaTime()
+          print("recieved tap at \(location)")
+        }
+      )
   }
 
   func updateTapLocation(_ location: CGPoint) {
