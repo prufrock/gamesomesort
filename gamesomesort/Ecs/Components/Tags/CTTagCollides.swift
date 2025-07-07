706f4456fa2f2ce2e39451fb4b4c78915190f9ef
David Kanenwisher <d.kanen@gmail.com>
2025-07-06 22:03:04 -0500

    Add CTTagCollides

---
+++ gamesomesort/Ecs/Components/Tags/CTTagCollides.swift
@@ -0,0 +1,12 @@
+//
+//  CTTagCollides.swift
+//  gamesomesort
+//
+//  Created by David Kanenwisher on 7/6/25.
+//
+
+import lecs_swift
+
+struct CTTagCollides: LECSComponent {
+  let exists = true
+}
