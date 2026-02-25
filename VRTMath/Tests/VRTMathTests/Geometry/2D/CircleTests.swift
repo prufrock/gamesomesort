0f5a80e4c1abdb3d9363ad5ea6ed19e4a2bc5730
David Kanenwisher <d.kanen@gmail.com>
2026-02-24 21:25:01 -0600

    Add a test for when circles don't intersect

--- VRTMath/Tests/VRTMathTests/Geometry/2D/CircleTests.swift
+++ VRTMath/Tests/VRTMathTests/Geometry/2D/CircleTests.swift
@@ -17,28 +17,34 @@ struct CircleTests {
     #expect(circleOne.area == .pi)
   }
 
-  @Test func `when circles intersect circles`() throws {
-    let circleOne = Circle(center: F2(0, 0), radius: 1.0)
+  @Test func `when circles intersect circles, or doesn't`() throws {
+    let circle = Circle(center: F2(0, 0), radius: 1.0)
 
     #expect(
-      circleOne.intersection(circleOne) == F2(2.0,0),
+      circle.intersection(circle) == F2(2.0,0),
       "the circles entirely overlap"
     )
 
-    let circleTwo = Circle(center: F2(1, 0), radius: 1.0)
+    let circleRightHalf = Circle(center: F2(1, 0), radius: 1.0)
     #expect(
-      circleOne.intersection(circleTwo) == F2(1.0,0),
+      circle.intersection(circleRightHalf) == F2(1.0,0),
       "the other circle overlaps the right half of the circle"
     )
 
-    let circleThree = Circle(center: F2(1, 1), radius: 1.0)
-    let upperQuarterResult = circleOne.intersection(circleThree)
+    let circleUpperQuarter = Circle(center: F2(1, 1), radius: 1.0)
+    let upperQuarterResult = circle.intersection(circleUpperQuarter)
     #expect(
       upperQuarterResult!.isClose(
         to: F2(0.414, 0.414),
         absoluteTolerance: 0.001
       ),
       "the other circle overlaps the upper right quarter of the circle"
+    )
+
+    let circleDoesntIntersect = Circle(center: F2(2,2), radius: 1.0)
+    #expect(
+      circle.intersection(circleDoesntIntersect) == nil,
+      "the circle shouldn't intersect"
     )
   }
 }
