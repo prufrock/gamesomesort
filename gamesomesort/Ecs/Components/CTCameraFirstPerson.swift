8342cfbb86029d13d7fecc2e22fe4c26dd19c313
David Kanenwisher <d.kanen@gmail.com>
2026-04-22 21:28:54 -0500

    Replace CTCameraFirstPerson with LECSPCameraFirstPerson

--- gamesomesort/Ecs/Components/CTCameraFirstPerson.swift
+++ gamesomesort/Ecs/Components/CTCameraFirstPerson.swift
@@ -6,21 +6,6 @@
 //
 
 import lecs_swift
+import LECSPieces
 
-struct CTCameraFirstPerson: LECSComponent {
-  var fov: Float
-  var nearPlane: Float
-  var farPlane: Float
-
-  init() {
-    self.fov = 1.0
-    self.nearPlane = 0.0
-    self.farPlane = 0.0
-  }
-
-  init(fov: Float, nearPlane: Float, farPlane: Float) {
-    self.fov = fov
-    self.nearPlane = nearPlane
-    self.farPlane = farPlane
-  }
-}
+typealias CTCameraFirstPerson = LECSPCameraFirstPerson
