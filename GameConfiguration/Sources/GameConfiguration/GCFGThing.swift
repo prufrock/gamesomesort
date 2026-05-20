01d1fefdda0827e71212b1f641d2f9c13ef35069
David Kanenwisher <d.kanen@gmail.com>
2026-05-19 22:20:20 -0500

    Add playerStart as a thing

--- GameConfiguration/Sources/GameConfiguration/GCFGThing.swift
+++ GameConfiguration/Sources/GameConfiguration/GCFGThing.swift
@@ -5,10 +5,42 @@
 //  Created by David Kanenwisher on 4/5/26.
 //
 
-public struct GCFGThing: Decodable {
-  public let name: String
+import VRTMath
 
-  public init(name: String) {
-    self.name = name
+public struct GCFGThing: Decodable {
+  public let color: F3
+  public let model: String
+  public let type: ThingTypes
+  public let radius: Float
+  public let rotationDegY: Float
+  public let scale: Float
+  public let tappable: Bool
+  public let visible: Bool
+  public let z: Float
+
+  public init(
+    color: F3,
+    model: String,
+    type: ThingTypes,
+    radius: Float,
+    rotationDegY: Float,
+    scale: Float,
+    tappable: Bool,
+    visible: Bool,
+    z: Float
+  ) {
+    self.color = color
+    self.model = model
+    self.type = type
+    self.radius = radius
+    self.rotationDegY = rotationDegY
+    self.scale = scale
+    self.tappable = tappable
+    self.visible = visible
+    self.z = z
+  }
+
+  public enum ThingTypes: String, Decodable {
+    case playerStart
   }
 }
