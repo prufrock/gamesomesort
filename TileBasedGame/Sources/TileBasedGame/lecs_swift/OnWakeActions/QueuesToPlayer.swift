9232475a170971eeeb641a0f4364cd2067fc4754
David Kanenwisher <d.kanen@gmail.com>
2026-06-20 22:02:15 -0500

    Refactor the wake up quote a little

---
+++ TileBasedGame/Sources/TileBasedGame/lecs_swift/OnWakeActions/QueuesToPlayer.swift
@@ -0,0 +1,14 @@
+//
+//  QueuesToPlayer.swift
+//  TileBasedGame
+//
+//  Created by David Kanenwisher on 6/20/26.
+//
+
+import LECSPieces
+
+extension LECSPOnWake {
+  static func queuesToPlayer() {
+    print("queuesToPlayer")
+  }
+}
--- TileBasedGame/Sources/TileBasedGame/lecs_swift/OnWakeActions/WakeUp.swift
+++ TileBasedGame/Sources/TileBasedGame/lecs_swift/OnWakeActions/WakeUp.swift
@@ -12,30 +12,35 @@ import lecs_swift
 extension LECSPOnWake {
   func wakeUp(id: LECSId, ecs: LECSWorld, worldCfg: GCFGWorld) {
     set.forEach { action in
-      switch action {
-      case .creates(creatureId: let creatureId):
-        print("creates creatureId: \(creatureId)")
-        Self.createsCreature(
-          id: id,
-          creature: worldCfg[creature: Int(creatureId)!]!,
-          ecs: ecs
-        )
-      case .queuesToPlayer:
-        print("queuesToPlayer")
-      case .createsMoveBtns(
-        up: let up, down: let down, left: let left, right: let right
-      ):
-        LECSPOnWake.createsMoveButtons(
-          id: id,
-          buttons: [
-            worldCfg[thing: up]!,
-            worldCfg[thing: down]!,
-            worldCfg[thing: left]!,
-            worldCfg[thing: right]!
-          ],
-          ecs: ecs
-        )
-      }
+      action.execute(sourceId: id, ecs: ecs, worldCfg: worldCfg)
+    }
+  }
+}
+
+extension LECSPOnWake.Action {
+  func execute(sourceId id: LECSId, ecs: LECSWorld, worldCfg: GCFGWorld) {
+    switch self {
+    case .creates(creatureId: let creatureId):
+      LECSPOnWake.createsCreature(
+        id: id,
+        creature: worldCfg[creature: Int(creatureId)!]!,
+        ecs: ecs
+      )
+    case .queuesToPlayer:
+      LECSPOnWake.queuesToPlayer()
+    case .createsMoveBtns(
+      up: let up, down: let down, left: let left, right: let right
+    ):
+      LECSPOnWake.createsMoveButtons(
+        id: id,
+        buttons: [
+          worldCfg[thing: up]!,
+          worldCfg[thing: down]!,
+          worldCfg[thing: left]!,
+          worldCfg[thing: right]!
+        ],
+        ecs: ecs
+      )
     }
   }
 }
