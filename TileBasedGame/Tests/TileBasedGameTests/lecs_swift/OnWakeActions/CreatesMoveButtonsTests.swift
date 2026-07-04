da5cbf8ec97409f00e26d1aa85dccc97c03b7d4b
David Kanenwisher <d.kanen@gmail.com>
2026-07-03 21:49:38 -0500

    remove test for creating the move buttons

--- TileBasedGame/Tests/TileBasedGameTests/lecs_swift/OnWakeActions/CreatesMoveButtonsTests.swift
+++ TileBasedGame/Tests/TileBasedGameTests/lecs_swift/OnWakeActions/CreatesMoveButtonsTests.swift
@@ -22,27 +22,4 @@ struct CreatesMoveButtonsTests {
     worldCfg = helpers.worldCfg
     helpers.initComponents(ecs: ecs)
   }
-
-  @Test func `creates a button relative to the source position`() {
-    let sourceId = ecs.createEntity("sourceEntity")
-    let sourcePos = LECSPPosition3d([1 , 2, 3])
-    ecs.addComponent(sourceId, sourcePos)
-
-    let thingCfg = worldCfg[thing: 2]!
-
-    LECSPOnWake.createsMoveButtons(
-      id: LECSId(sourceId),
-      buttons: [thingCfg],
-      ecs: ecs
-    )
-
-    var pos: LECSPPosition3d? = nil
-    ecs.select([LECSName.self, LECSPPosition3d.self]) { rows, columns in
-      let count = counter()
-      let name = rows.component(at: count(), columns, LECSName.self)
-      if name.name.hasPrefix("thing-\(thingCfg.type)") {
-        pos = rows.component(at: count(), columns, LECSPPosition3d.self)
-      }    }
-    #expect(pos!.position.isClose(to: sourcePos.position + thingCfg.position))
-  }
 }
